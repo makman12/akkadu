2be31391bd9fe3aae069d9eb052e115b3af51496
makman12 <makman12h@gmail.com>
2020-10-02 11:42:31 +0300

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main">
+    <h1>{{ deneme }}</h1>
+    <button @click="add()">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      deneme: 1,
+    };
+  },
+  methods: {
+    add() {
+      this.deneme++;
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  padding: 1%;
+  border: solid rebeccapurple;
+}
+
+.main {
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  align-content: space-around;
+}
+</style>
